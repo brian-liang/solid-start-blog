d6039981f176ee1d7fffc954def1c941f1a31f11
brian-liang <brian.j.liang@gmail.com>
2023-02-24 13:47:51 -0700

    Typing for ArticleHead

--- src/components/ArticleHead.tsx
+++ src/components/ArticleHead.tsx
@@ -1,11 +1,17 @@
 import { Head, Title, Meta, Link, Body, Scripts } from "solid-start";
 
-const ArticleHead = (props) =>{
+type articleHeadProps = {
+    title: string,
+    description: string
+    date: string
+}
+
+const ArticleHead = (props : articleHeadProps) =>{
     return(
         <Head>
             <Title>{props.title} - Solid Blog</Title>
             <Meta name="description" content={props.description}/>
-            <Meta property='og:description' content={props.descripton}/>
+            <Meta property='og:description' content={props.description}/>
             <Meta property='og:title' content={`${props.title} - Solid Blog`}/>
         </Head>
     )
